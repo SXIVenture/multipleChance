4eeb9558ab16fe01d06630812789b6d8f586f2d0
SatakeYusuke19920527 <satkyusk@icloud.com>
2020-01-23 23:42:48 +0900

    完了していないけど、一旦commmit

--- component/Profile.js
+++ component/Profile.js
@@ -1,35 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, View, Image, Text, ActivityIndicator } from 'react-native';
 import firebase from 'firebase';
 
 class Profile extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      user: '',
-    }
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: 'Satake Yusuke',
+			profession: '塾長'
+		};
+	}
 
-  _getUserName = () => {
-    const user = firebase.auth().currentUser
-    this.setState({
-      user: user.email,
-    });
-}
+	_getUserName = () => {
+		const user = firebase.auth().currentUser;
+		this.setState({
+			user: user.email
+		});
+	};
 
-  render(){
-    return (
-      <View style={styles.container}>
-        <Button
-          title='get emailAddress'
-          onPress={
-            () => {this._getUserName()}
-          }
+	render() {
+		return (
+			<View style={styles.container}>
+				<View style={styles.imgArea}>
+					<Image
+          rounded
+          source={require('../assets/React-icon-mini2.png')}
+          style={styles.imageArea}  
         />
-        <Text>{this.state.user}</Text>
-      </View>
-    );
-  }
+				</View>
+				<View style={styles.infoContent}>
+					<Text style={styles.name}>{this.state.name}</Text>
+					<Text style={styles.profession}>{this.state.profession}</Text>
+				</View>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
@@ -38,8 +43,24 @@ const styles = StyleSheet.create({
 		backgroundColor: 'white',
 		alignItems: 'center',
 		justifyContent: 'center'
+	},
+	imageArea: {
+		width: 200,
+    height: 200,
+	},
+	infoContent: {
+		marginTop: 100,
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	name: {
+		fontSize: 30,
+		fontWeight: '500'
+	},
+	profession: {
+		fontSize: 25,
+		fontWeight: '200'
 	}
 });
-
 
 export default Profile;
