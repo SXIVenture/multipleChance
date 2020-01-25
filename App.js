770153725036db9611a3a290909913c400ff7bb9
SatakeYusuke19920527 <satkyusk@icloud.com>
2020-01-25 15:23:59 +0900

    Firebaseの情報を取り除く

--- App.js
+++ App.js
@@ -4,20 +4,7 @@ import firebase from 'firebase';
 import PageController from './component/PageController';
 import Login from './screen/LoginScreen';
 
-// firebase 読み込み
-// Your web app's Firebase configuration
-var firebaseConfig = {
-	apiKey: 'AIzaSyBhfHunJmyca266zrGQGBlcdxxPT5Y9bbY',
-	authDomain: 'multiplechance-57751.firebaseapp.com',
-	databaseURL: 'https://multiplechance-57751.firebaseio.com',
-	projectId: 'multiplechance-57751',
-	storageBucket: 'multiplechance-57751.appspot.com',
-	messagingSenderId: '323568257253',
-	appId: '1:323568257253:web:568282f4ef0b0cf19aec0c',
-	measurementId: 'G-F95VRFD4R3'
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// ここにFirebaseの情報をペーズト
 
 const AppNavigator = createStackNavigator(
 	{
