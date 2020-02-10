e209f16d10b270492b6f7ea2b00eb44c9158c81a
keid131 <keid951022@gmail.com>
2020-02-10 16:05:22 +0900

    react-native-dotenvの追加

--- config/firebase.js
+++ config/firebase.js
@@ -1,17 +1,27 @@
 import firebase from 'firebase';
+import {
+	API_KEY,
+	AUTH_DOMAIN,
+	DATABASE_URL,
+	PROJECT_ID,
+	STORAGE_BUCKET,
+	MESSAGING_SENDER_ID,
+	APP_ID,
+	MEASUREMENT_ID
+  } from 'react-native-dotenv';
 import 'firebase/auth';
 import 'firebase/firestore';
 // firebase 読み込み
 // Your web app's Firebase configuration
 var firebaseConfig = {
-	apiKey: '',
-	authDomain: '',
-	databaseURL: '',
-	projectId: '',
-	storageBucket: '',
-	messagingSenderId: '',
-	appId: '',
-	measurementId: ''
+	apiKey: API_KEY,
+	authDomain: AUTH_DOMAIN,
+	databaseURL: DATABASE_URL,
+	projectId: PROJECT_ID,
+	storageBucket: STORAGE_BUCKET,
+	messagingSenderId: MESSAGING_SENDER_ID,
+	appId: APP_ID,
+	measurementId: MEASUREMENT_ID
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
