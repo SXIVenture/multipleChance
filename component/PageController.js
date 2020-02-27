1cc66a98e096d168c7685d7a991341d5c9c58b15
Unknown <idsaikyo@icloud.com>
2020-02-27 23:03:08 +0900

    チャットページの作成
    一部DBと繋ぎこみ

--- component/PageController.js
+++ component/PageController.js
@@ -1,12 +1,20 @@
 import Icon from '@expo/vector-icons/FontAwesome';
 import React from 'react';
 import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import MapScreen from '../screen/MapScreen';
-import MessageScreen from '../screen/MessageScreen';
 import HomeScreen from '../screen/HomeScreen';
+import MessageScreen from '../screen/MessageScreen';
+import ChatRoomScreen from '../screen/ChatRoomScreen';
 
-
+const ChatStack = createStackNavigator(
+  {
+    Message: { screen: MessageScreen},
+    Chat: { screen: ChatRoomScreen},
+  },
+  {initialRouteName: 'Chat', mode: 'card', headerMode: 'none'}
+  );
 const BottomTabNavigatorConfig = {
   initialRouteName: 'Profile',
   tabBarOptions: { showLabel: false }
@@ -31,8 +39,8 @@ const TabNavigator = createAppContainer(
           )
         })
       },
-      Message: {
-        screen: MessageScreen,
+      ChatRoom: {
+        screen: ChatStack,
         navigationOptions: () => ({
           tabBarIcon: ({ tintColor }) => (
             <Icon name='send-o' color={tintColor} size={24} />
--- screen/MessageScreen.js
+++ screen/MessageScreen.js
@@ -1,48 +1,101 @@
 import React from 'react';
+import { AsyncStorage } from 'react-native';
+import firebase from '../config/firebase';
 import { GiftedChat } from 'react-native-gifted-chat';
+import { Container, Header, Left, Body, Right, Button, Icon, Title, Text } from 'native-base';
 
 class MessageScreen extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {
-            message: [],
-        }
     }
 
-    componentWilllMount() {
-        this.setState({ messages: [] });
+    state = {
+        messages: [],
+        uid: ''
     }
 
-    reply() {
-        return {
-            _id: 1,
-            text: 'こんにちは( ͡° ͜ʖ ͡°)',
-            createdAt: new Date(),
-            user: {
-                _id: 2,
-                name: 'React Native',
-                avatar: 'https://placeimg.com/140/140/any',
+    componentDidMount() {
+        // Firestoreの「messages」コレクションを参照
+        this.messageRef = firebase.firestore().collection('messages');
+        // refの更新時イベントにonCollectionUpdate登録
+        this.unsubscribe = this.messageRef.onSnapshot(this.onCollectionUpdate);
+        // uidを取得
+        const uid=this._getUid()
+        this.setState({uid})
+    }
+
+    _getUid = async () => {
+        const userToken = await AsyncStorage.getItem('userUid');
+        return userToken
+      };
+
+    componentWillunmount() {
+        // onCollectionUpdateの登録解除
+        this.unsubscribe();
+    }
+
+    /*Firestoreのコレクションが更新されたとき*/
+    onCollectionUpdate = (querySnapshot) => {
+        // docsのdataをmessagesとして取得
+        const messages = querySnapshot.docs.map((doc) => {
+            return doc.data();
+        }).sort(
+            function (a, b) {
+                return (a._id < b._id ? 1 : -1);
             }
-        };
+        );
+        // メッセージ時間の型変換
+        messages.forEach(function(data){
+            data.createdAt=data.createdAt.toDate()
+        });
+        this.setState({ messages });
     }
 
-    onSend(messages = []) {
-        this.setState(previousState => ({
-            messages: GiftedChat.append(GiftedChat.append(previousState.messages, messages), this.reply()),
-        }))
+    _backToChatRoomScreen =() => {
+        this.props.navigation.navigate('Chat');
     }
+    // componentWilllMount() {
+    //     this.setState({ messages: [] });
+    // }
+
+    onSend = (messages = []) => {
+        // Firestoreのコレクションに追加
+        messages.forEach((message) => {
+            var dt = new Date();
+            message._id=dt.getTime()+message._id;
+            this.messageRef.add(message);
+        });
+        // onCollectionUpdateが呼ばれるので、ここではstateには渡さない
+        //this.setState((previousState) => ({
+        //  messages: GiftedChat.append(previousState.messages, messages),
+        //}));
+      }
 
     render() {
         return (
+            <Container>
+            <Header>
+                <Left>
+                    <Button transparent
+                    onPress ={() => this._backToChatRoomScreen()}
+                    >
+                        <Icon name='arrow-back' />
+                    </Button>
+                </Left>
+                <Body>
+                <Title>Header</Title>
+                </Body>
+                <Right>
+                </Right>
+            </Header>
             <GiftedChat
                 messages={this.state.messages}
                 onSend={messages => this.onSend(messages)}
                 user={{
                     _id: 1,
-                    name: 'this.',
-                    avater: 'https://placeimg.com/140/140/any'
                 }}
             />
+            </Container>
         );
     }
 }
